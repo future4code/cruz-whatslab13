2932337a2b5c507563e41f2b6f6ef54d91a7eab6
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-03-12 18:28:39 -0300

    projeto estilizado e finalizado

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -2,6 +2,43 @@ import React from 'react'
 import './App.css';
 import styled from 'styled-components'
 
+
+const ContainerPrincipal = styled.div`
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction: column;
+`
+
+const ContainerMensagens = styled.div`
+  border: 1px solid black;
+  height: 100vh;
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  background-color: beige
+`
+
+const Imputs = styled.div`
+  display:flex;
+  width: 100%;
+  height: 40px
+`
+
+const ImputNome = styled.input`
+  width:20%;
+`
+
+const ImpuTexto = styled.input`
+width:60%;
+`
+
+const ButtonEstilizado = styled.button`
+  width:20%;
+  background-color: #34af23;
+`
+
 class App extends React.Component {
   state = {
     valorImputNome: "",
@@ -36,26 +73,28 @@ const
 
     const mensagensEnviadas = this.state.mensagens.map((mensagem) => {
       return(
-        <p>{mensagem.nome}: {mensagem.texto}</p>
+        <p><b>{mensagem.nome}:</b> {mensagem.texto}</p>
       )
     })
 
     return (
-      <div>
+      <ContainerPrincipal>
         
-        <div>
+        <ContainerMensagens>
           {mensagensEnviadas}
-        </div>
-
-        <input value={this.state.valorImputNome}  onChange={this.onChangeInputNome} placeholder={"Usuário"}/>
+        
+          <Imputs>
+          <ImputNome value={this.state.valorImputNome}  onChange={this.onChangeInputNome} placeholder={"Usuário"}/>
         
         
-        <input value={this.state.valorImputTexto}  onChange={this.onChangeInputTexto} placeholder={"Digite algo"}/>
+          <ImpuTexto value={this.state.valorImputTexto}  onChange={this.onChangeInputTexto} placeholder={"Digite algo"}/>
 
         
 
-        <button onClick={this.enviarMensagem}> ENVIAR </button>
-      </div>
+          <ButtonEstilizado onClick={this.enviarMensagem}> ENVIAR </ButtonEstilizado>
+          </Imputs>
+        </ContainerMensagens>
+      </ContainerPrincipal>
   )
 
   }
